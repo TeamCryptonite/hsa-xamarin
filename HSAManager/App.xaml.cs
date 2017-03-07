ab794746d55d7239f2060bba61562218adf8bed6
Seth vonSeggern <savtk5@mail.missouri.edu>
2017-03-07 14:54:20 -0600

    backend authentication set up

--- HSAManager/App.xaml.cs
+++ HSAManager/App.xaml.cs
@@ -1,19 +1,50 @@
-﻿using Xamarin.Forms;
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
 
 namespace HSAManager
 {
 	public partial class App : Application
 	{
+		// the todos (databinded from TasksPage.XAML)
+		public static ObservableCollection<string> Tasks { get; set; }
+		// the app
+		public static PublicClientApplication PCApplication { get; set; }
+
+		// app coordinates
+		public static string ClientID = "6a99c57e-3489-4e91-a398-e93f8cec4af8";
+		public static string[] Scopes = { ClientID };
+
+
+		public static string SignUpSignInpolicy = "B2C_1_HSA_SignUp_SignIn_Default";
+		public static string ResetPasswordpolicy = "b2c_1_reset";
+		public static string Authority = "https://login.microsoftonline.com/cryptonitehsaservice.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1_HSA_SignUp_SignIn_Default";
+		public static string APIbaseURL = "https://bizzaro.azurewebsites.net";
+
 		public App()
 		{
-			InitializeComponent();
+			//InitializeComponent();
 
-			MainPage = new NavigationPage(new HSAManagerPage())
-			{
-				BarBackgroundColor = Color.FromHex("#00cccc"),
-				BarTextColor = Color.White
-			};
+			//MainPage = new NavigationPage(new HSAManagerPage())
+			//{
+			//	BarBackgroundColor = Color.FromHex("#00cccc"),
+			//	BarTextColor = Color.White
+			//};
 
+			Tasks = new ObservableCollection<string>();
+
+			PCApplication = new PublicClientApplication(Authority, ClientID);
+			//{
+			//    RedirectUri = redirectURI,
+			//};
+
+			// The root page of your application
+			MainPage = new NavigationPage(new HSAManager.HSAManagerPage());
 		}
 
 		protected override void OnStart()
---
+++ iOS/HSAManagerPageRenderer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using HSAManager;
+using HSAManager.iOS;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(HSAManagerPage), typeof(HSAManagerPageRenderer))]
+namespace HSAManager.iOS
+{
+	class HSAManagerPageRenderer : PageRenderer
+	{
+		HSAManagerPage page;
+		protected override void OnElementChanged(VisualElementChangedEventArgs e)
+		{
+			base.OnElementChanged(e);
+			page = e.NewElement as HSAManagerPage;
+		}
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			page.platformParameters = new PlatformParameters(this);
+		}
+	}
+}
