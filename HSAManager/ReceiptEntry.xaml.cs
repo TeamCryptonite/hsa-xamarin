d9c6d1766f6a191d328c123889ff986dcde73fbf
Seth vonSeggern <savtk5@mail.missouri.edu>
2017-03-12 22:42:57 -0500

    exploration of an alternative to a list view on manual receipt entry page

--- HSAManager/ReceiptEntry.xaml.cs
+++ HSAManager/ReceiptEntry.xaml.cs
@@ -7,9 +7,58 @@ namespace HSAManager
 {
 	public partial class ReceiptEntry : ContentPage
 	{
+
+		public class lineItem
+		{
+			public lineItem(string name, double price)
+			{
+				this.Name = name;
+				this.Price = price;
+			}
+
+			public string Name { private set; get;}
+			public double Price { private set; get; }
+		}
+
+
+		List<lineItem> receiptItems = new List<lineItem>();
+
+
 		public ReceiptEntry()
 		{
 			InitializeComponent();
+		}
+
+		void getBlob(object sender, System.EventArgs e)
+		{
+			// get image file type and retrieve blob
+		}
+
+		void addItems(object sender, System.EventArgs e)
+		{
+			lineItem lI = new lineItem(item.Text, Double.Parse(price.Text));
+
+			System.Diagnostics.Debug.WriteLine(lI.Name + " " + lI.Price);
+			receiptItems.Add(lI);
+			for (int x = 0; x <= receiptItems.Count-1; x++) 
+			{
+				if (x < 1)
+				{
+					itemsBox.Text= receiptItems[x].Name + "                                         " + receiptItems[x].Price;
+				}
+				else
+				{
+					itemsBox.Text += "\n" + receiptItems[x].Name + "                                         " + receiptItems[x].Price;
+				}
+
+			}
+			//itemsBox.Text(lI.Name, lI.Price);
+			
+		}
+
+		void submitReceipt(object sender, System.EventArgs e)
+		{
+			
 		}
 	}
 }
