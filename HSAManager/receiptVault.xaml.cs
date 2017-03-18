793f8eea90b29cd7c1d830c73b069d489da18b02
Pearse Hutson <pearse.2008@gmail.com>
2017-03-18 17:23:37 -0500

    Implemented Search in receipts.

--- HSAManager/receiptVault.xaml.cs
+++ HSAManager/receiptVault.xaml.cs
@@ -1,50 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 using HsaServiceDtos;
 using Xamarin.Forms;
+
 //using HsaServiceDtos;
 
 namespace HSAManager
 {
-	public partial class receiptVault : ContentPage
-	{
-		public ObservableCollection<HsaServiceDtos.ReceiptDto> receipts;
+    public partial class receiptVault : ContentPage
+    {
+        private BizzaroClient client;
+        private CancellationTokenSource cts;
+        public ObservableCollection<ReceiptDto> receipts;
+        private Paginator<ReceiptDto> ReceiptsPaginator;
 
-		public receiptVault()
-		{
-			InitializeComponent();
+        public receiptVault()
+        {
+            InitializeComponent();
             receipts = new ObservableCollection<ReceiptDto>();
             listView.ItemsSource = receipts;
+
+            listView.ItemAppearing += async (sender, e) =>
+            {
+                if (receipts.Count == 0)
+                    return;
+                cts = new CancellationTokenSource();
+                if (((ReceiptDto) e.Item).ReceiptId == receipts[receipts.Count - 1].ReceiptId)
+                    try
+                    {
+                        await AddNewReceipts(cts.Token);
+                    }
+                    catch
+                    {
+                    }
+            };
         }
 
-	    protected override async void OnAppearing()
-	    {
+        protected override async void OnAppearing()
+        {
             if (Application.Current.Properties.ContainsKey("authKey"))
             {
                 //string authKey = Application.Current.Properties["authKey"].ToString();
-                var client = new BizzaroClient();
+                client = new BizzaroClient();
 
-                var receiptsPaginator = client.Receipts.GetListOfReceipts();
-                var receiptsToAdd = await receiptsPaginator.Next();
-
-                foreach (var receipt in receiptsToAdd)
+                ReceiptsPaginator = client.Receipts.GetListOfReceipts();
+                cts = new CancellationTokenSource();
+                try
                 {
-                    receipts.Add(receipt);
+                    await AddNewReceipts(cts.Token);
+                }
+                catch
+                {
                 }
             }
         }
 
-	    void searchChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
-		{
-			
-		}
+        protected async void searchChanged(object sender, TextChangedEventArgs e)
+        {
+            cts.Cancel();
+            receipts.Clear();
+            ReceiptsPaginator = client.Receipts.GetListOfReceipts(e.NewTextValue);
+            cts = new CancellationTokenSource();
+            try
+            {
+                await AddNewReceipts(cts.Token);
+            }
+            catch
+            {
+            }
+        }
 
-		//public async void Handle_Tapped(object sender, System.EventArgs e)
-		//{
-		//	await Navigation.PushAsync(new data());
-		//}
-	}
+        protected async Task AddNewReceipts(CancellationToken cToken)
+        {
+            Debug.WriteLine("Addding New Receipts");
+            var receiptsToAdd = await ReceiptsPaginator.Next();
+            cToken.ThrowIfCancellationRequested();
+            foreach (var receipt in receiptsToAdd)
+            {
+                cToken.ThrowIfCancellationRequested();
+                receipts.Add(receipt);
+            }
+        }
+
+        //public async void Handle_Tapped(object sender, System.EventArgs e)
+        //{
+        //	await Navigation.PushAsync(new data());
+
+        //}
+    }
 }
