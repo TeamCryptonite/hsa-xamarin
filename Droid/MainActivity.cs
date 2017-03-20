246aae0df4256c3f72a789e3acf0d2fde8ad0fdc
Unknown <cdivff@mail.missouri.edu>
2017-03-20 11:20:27 -0500

    Droid camera access

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -7,17 +7,18 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
-
+using Plugin.Media;
 namespace HSAManager.Droid
 {
 	[Activity(Label = "HSAManager.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
-		protected override void OnCreate(Bundle bundle)
+		protected override async void OnCreate(Bundle bundle)
 		{
 			TabLayoutResource = Resource.Layout.Tabbar;
 			ToolbarResource = Resource.Layout.Toolbar;
 
+			await CrossMedia.Current.Initialize();
 			base.OnCreate(bundle);
 
 			global::Xamarin.Forms.Forms.Init(this, bundle);
