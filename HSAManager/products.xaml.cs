f12020a54fb39d84e9db2fa64080de4d7f547a44
Steven Snow <stsnqf@mail.missouri.edu>
2017-03-22 18:51:31 -0500

    Fixed DateTime

--- HSAManager/products.xaml.cs
+++ HSAManager/products.xaml.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using HsaServiceDtos;
 using Xamarin.Forms;
 
 namespace HSAManager
 {
 	public partial class products : ContentPage
 	{
+		
 		public products()
 		{
 			InitializeComponent();
 		}
+
 	}
 }
