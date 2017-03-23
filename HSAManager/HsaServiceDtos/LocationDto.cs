cf79158b67b1c8bd620b8034a707270f8a634bd8
Pearse Hutson <pearse.2008@gmail.com>
2017-03-23 14:48:03 -0500

    Fix for Lat and Long error when searching for stores.

--- HSAManager/HsaServiceDtos/LocationDto.cs
+++ HSAManager/HsaServiceDtos/LocationDto.cs
@@ -6,7 +6,19 @@ namespace HsaServiceDtos
 {
     public class LocationDto
     {
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+
+        public bool IsValidLocation()
+        {
+            if (Latitude == null || Longitude == null)
+                return false;
+            if (Latitude < -90 || Latitude > 90)
+                return false;
+            if (Longitude < -180 || Longitude > 180)
+                return false;
+
+            return true;
+        }
     }
 }
