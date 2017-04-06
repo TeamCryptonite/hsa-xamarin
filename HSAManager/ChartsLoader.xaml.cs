ceaf1e9a0adea33890496fa915c54941d2727f79
Unknown <cdivff@mail.missouri.edu>
2017-04-05 19:00:15 -0500

    WebView Support
    Charts button sends the user to a webpage ready to be directed to our own charts webpage.

---
+++ HSAManager/ChartsLoader.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HSAManager
+{
+	public partial class ChartsLoader : ContentPage
+	{
+		public ChartsLoader()
+		{
+			InitializeComponent();
+		}
+	}
+}
