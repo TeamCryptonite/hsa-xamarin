7a5fe3af7c25852d136a39c58e3bcdd0317349a1
Seth vonSeggern <savtk5@mail.missouri.edu>
2017-04-08 17:07:26 -0500

    updating product name, updating store name, working on navigation

--- HSAManager/ReceiptView.xaml.cs
+++ HSAManager/ReceiptView.xaml.cs
@@ -17,7 +17,7 @@ namespace HSAManager
         {
             InitializeComponent();
             receipt = receiptInput;
-            store.Text = receipt.Store != null ? receipt.Store?.Name + " Receipt" : "No Store For This Receipt";
+            store.Text = receipt.Store != null ? receipt.Store?.Name : "No Store";
             date.Text = receipt.DateTime != null
                 ? $"{receipt.DateTime:MMMM d, yyyy}"
                 : "No Date For This Receipt";
@@ -46,33 +46,58 @@ namespace HSAManager
             var bindingContext = (Xamarin.Forms.BindableObject)entry.Parent.Parent;
             var lineItem = bindingContext.BindingContext as LineItemDto;
             lineItem.Product.Name = name;
+			lineItem.Product.ProductId = 0;
             await client.Receipts.UpdateReceiptListItem(receipt.ReceiptId, lineItem);
         }
 
-        private async void Price_Entry_Unfocused(object sender, FocusEventArgs e)
-        {
-            var client = new BizzaroClient();
-            var entry = (Xamarin.Forms.Entry)sender;
-            var price = entry.Text;
-            var bindingContext = (Xamarin.Forms.BindableObject)entry.Parent.Parent;
-            var lineItem = bindingContext.BindingContext as LineItemDto;
-            try
-            {
-                if (price != "")
-                {
-                    lineItem.Price = Convert.ToDecimal(price);
-                    await client.Receipts.UpdateReceiptListItem(receipt.ReceiptId, lineItem);
-                }
-                else
-                {
-                    lineItem.Price = 0;
-                }
-            }
-            catch (Exception ex)
-            {
-                entry.Text = "";
-                await DisplayAlert("Oops", "Please enter a valid price. Ex: 1.00, 5.35, 2, etc.", "OK");
-            }
-        }
+		private async void Price_Entry_Unfocused(object sender, FocusEventArgs e)
+		{
+			var client = new BizzaroClient();
+			var entry = (Xamarin.Forms.Entry)sender;
+			var price = entry.Text;
+			var bindingContext = (Xamarin.Forms.BindableObject)entry.Parent.Parent;
+			var lineItem = bindingContext.BindingContext as LineItemDto;
+			try
+			{
+				if (price != "")
+				{
+					lineItem.Price = Convert.ToDecimal(price);
+					await client.Receipts.UpdateReceiptListItem(receipt.ReceiptId, lineItem);
+				}
+				else
+				{
+					lineItem.Price = 0;
+				}
+			}
+			catch (Exception ex)
+			{
+				entry.Text = "";
+				await DisplayAlert("Oops", "Please enter a valid price. Ex: 1.00, 5.35, 2, etc.", "OK");
+			}
+		}
+
+		private async void Store_Entry_Unfocused(object sender, FocusEventArgs e)
+		{
+			var client = new BizzaroClient();
+			var entry = (Xamarin.Forms.Entry)sender;
+			receipt.Store.Name = entry.Text;
+			receipt.Store.StoreId = 0;
+			await client.Receipts.UpdateReceipt(receipt.ReceiptId, receipt);
+		}
+
+		protected override void OnDisappearing()
+		{
+			Navigation.PopToRootAsync();
+			Navigation.PushAsync(new receiptVault());
+			//foreach (Page page in Navigation.NavigationStack)
+			//{
+			//	if (page typeof(HSAManager.receiptVault)){
+					
+			//	}
+			//}
+			//Navigation.InsertPageBefore
+			//Navigation.PushAsync(new receiptVault());
+		}
+			
     }
 }
