eecbb4c5f516833506ae9e6792f28f276cedbf83
Pearse Hutson <pearse.2008@gmail.com>
2017-04-17 16:17:38 -0500

    Fixed some of the jumpyness. Allows Start and End Dates
    Need to stop it from jumping twice when first loading chart.

--- HSAManager/Helpers/BizzaroHelpers/BizzaroAggregate.cs
+++ HSAManager/Helpers/BizzaroHelpers/BizzaroAggregate.cs
@@ -8,6 +8,10 @@ namespace HSAManager.Helpers.BizzaroHelpers
 {
     public class BizzaroAggregate : AbstractBizzaroActions
     {
+        public enum TimePeriod
+        {
+            YearMonth, Day, Month, Year
+        }
         public BizzaroAggregate()
         {
         }
@@ -16,9 +20,15 @@ namespace HSAManager.Helpers.BizzaroHelpers
         {
         }
 
-        public async Task<string> GetSpendingOverTime()
+        public async Task<string> GetSpendingOverTime(DateTime? startDateStr = null, DateTime? endDateStr = null, TimePeriod? timePeriod = null)
         {
             var request = new RestRequest("receiptaggregate/spendingovertime", Method.GET);
+            if (startDateStr.HasValue)
+                request.AddOrUpdateQueryParameter("startDateStr", startDateStr.Value.ToString());
+            if(endDateStr.HasValue)
+                request.AddOrUpdateQueryParameter("endDateStr", endDateStr.Value.ToString());
+            if (timePeriod.HasValue)
+                request.AddOrUpdateQueryParameter("timePeriod", timePeriod.ToString().ToLower());
 
             var json = await CallBizzaroJArray(request);
             return json.ToString();
